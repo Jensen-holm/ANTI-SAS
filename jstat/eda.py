8039d6c106de2ee27d2efdf43fa2e326f7a07a0d
Jensen Holm <96762808+Jensen-holm@users.noreply.github.com>
2022-11-22 10:11:39 -0500

    sort of got the horizontal qq plot line to work

--- jstat/eda.py
+++ jstat/eda.py
@@ -28,14 +28,29 @@ def histogram(x, x_lab="", y_lab="", w=25, h=15, label=""):
 
 def qq_plot(y, lab="", w=25, h=15):
     x = standardize_arr(y)
+    x.sort()
+    x = [[i/len(x), v] for i, v in enumerate(x)]
+
+    xaxis = [v[1] for v in x]
+    yaxis = [v[0] for v in x]
+
     fig = plotille.Figure()
     fig.width = w
     fig.height = h
     fig.x_label = "Theoritical quantities (z-scores)"
-    fig.y_label = "Actual values"
-    fig.scatter(x, y, label=lab, lc="blue")
+    fig.y_label = "sample values"
+    fig.scatter(xaxis, yaxis, label=lab, lc="blue")
 
     # make the 45 degree line
-    q = np.arange(max(x))
-    fig.plot(q, q, label="qq line", lc="red")
+
+    
+
+    x = np.arange(min(xaxis), max(xaxis), (max(xaxis) - min(xaxis)) / len(xaxis))
+    y = np.arange(min(yaxis), max(yaxis), (max(yaxis) - min(yaxis)) / len(yaxis))
+
+    print(x)
+    print(y)
+
+    fig.plot(x, y, label="qq line", lc="red")
+
     print(fig.show(legend=True))
